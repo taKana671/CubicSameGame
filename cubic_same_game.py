269df61bdb9cd0e73a881d99cfeaf6bac66c257d
taKana671 <kanae5321@gmail.com>
2022-06-19 11:07:17 +0900

    Modified to be able to click on a sphere.

--- cubic_same_game.py
+++ cubic_same_game.py
@@ -5,11 +5,15 @@ from enum import Enum
 from direct.showbase.InputStateGlobal import inputState
 from direct.showbase.ShowBaseGlobal import globalClock
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import NodePath, PandaNode, Quat, Vec3, LColor
+from panda3d.core import NodePath, PandaNode, Quat, Vec3, LColor, BitMask32
+from panda3d.core import CollisionTraverser, CollisionNode
+from panda3d.core import CollisionHandlerQueue, CollisionRay
 
 from window import Window
-from sphere import Sphere
 from lights import BasicAmbientLight, BasicDayLight
+
+
+PATH_SPHERE = 'models/alice-shapes--sphere/sphere'
 
 
 TURN_UP = 'TurnUp'
@@ -34,47 +38,97 @@ class Color(Enum):
         return random.sample(list(map(lambda c: c.value, cls)), n)
 
 
+class Sphere:
+
+    def __init__(self, node_path, tag, color, pos):
+        """color: LColor
+           pos: Vec3
+        """
+        self.model = base.loader.loadModel(PATH_SPHERE)
+        self.model.reparentTo(node_path)
+        self.model.setScale(0.2)
+        self.model.setColor(color)
+        self.model.setPos(pos)
+
+        self.model.find('**/Sphere').node().setIntoCollideMask(BitMask32.bit(1))
+        self.model.find('**/Sphere').node().setTag('sphere', str(tag))
+        # render/squareRoot/square.egg/polygon
+
+    def rotate_around(self, angle, axis, point):
+        object_pos = self.model.getPos()
+        q = Quat()
+        q.setFromAxisAngle(angle, axis.normalized())
+        r = q.xform(object_pos - point)
+        rotated_pos = point + r
+        self.model.setPos(rotated_pos)
+
+
 class Game(ShowBase):
 
     def __init__(self):
         super().__init__()
-        self.setBackgroundColor(1, 1, 1)
         self.disableMouse()
         self.camera.setPos(20, -20, 15)
         self.camera.lookAt(0, 0, 0)
-
-        # self.camera.setPos(20, -30, 5)
-        # self.camera.lookAt(0, 20, 5)
 
         self.wind = Window('CubicSameGame')
         self.ambient_light = BasicAmbientLight()
         self.directional_light = BasicDayLight()
 
-        self.create_key_controls()
+        self.setup_controls()
+        self.setup_collision_detection()
 
         self.base_point = Vec3(0, 0, 0)
         self.colors = Color.select(4)
         self.spheres = [[[None for _ in range(4)] for _ in range(4)] for _ in range(4)]
         self.pos = [-3, -1, 1, 3]
-        self.create_spheres()
+        self.setup_spheres()
 
         self.taskMgr.add(self.update, 'update')
 
-    def create_key_controls(self):
+    def setup_collision_detection(self):
+        self.picker = CollisionTraverser()
+        self.handler = CollisionHandlerQueue()
+
+        self.picker_node = CollisionNode('mouseRay')
+        self.picker_np = self.camera.attachNewNode(self.picker_node)
+        self.picker_node.setFromCollideMask(BitMask32.bit(1))
+        self.picker_ray = CollisionRay()
+        self.picker_node.addSolid(self.picker_ray)
+        self.picker.addCollider(self.picker_np, self.handler)
+
+    def setup_controls(self):
+        self.accept('mouse1', self.click)
         self.accept("escape", sys.exit)
         inputState.watchWithModifiers(TURN_UP, 'arrow_up')
         inputState.watchWithModifiers(TURN_DOWN, 'arrow_down')
         inputState.watchWithModifiers(TURN_LEFT, 'arrow_left')
         inputState.watchWithModifiers(TURN_RIGHT, 'arrow_right')
 
-    def create_spheres(self):
+    def setup_spheres(self):
+        self.sphere_root = self.render.attachNewNode('sphereRoot')
+        i = 0
+
         for x in range(4):
             for y in range(4):
                 for z in range(4):
                     idx = random.randint(0, 3)
                     pos = Vec3(self.pos[x], self.pos[y], self.pos[z])
-                    sphere = Sphere(self.colors[idx], pos)
+                    sphere = Sphere(self.sphere_root, i, self.colors[idx], pos)
                     self.spheres[x][y][z] = sphere
+                    i += 1
+
+    def click(self):
+        if self.mouseWatcherNode.hasMouse():
+            pos = self.mouseWatcherNode.getMouse()
+            self.picker_ray.setFromLens(self.camNode, pos.getX(), pos.getY())
+
+            self.picker.traverse(self.sphere_root)
+            # import pdb; pdb.set_trace()
+            if self.handler.getNumEntries() > 0:
+                self.handler.sortEntries()
+                idx = int(self.handler.getEntry(0).getIntoNode().getTag('sphere'))
+                print(idx)
 
     def update(self, task):
         dt = globalClock.getDt()
