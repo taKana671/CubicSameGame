189fcdc9321831bb6f318c04ba962c88e57fa95d
taKana671 <kanae5321@gmail.com>
2022-07-24 12:48:33 +0900

    First commit.

---
+++ window.py
@@ -0,0 +1,18 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
+
+
+class Window:
+
+    def __init__(self, title):
+        self.props = WindowProperties()
+        self.props.setTitle(title)
+        self.props.setSize(800, 600)
+        base.win.requestProperties(self.props)
+        base.setBackgroundColor(0, 0, 0)
+
+
+if __name__ == '__main__':
+    base = ShowBase()
+    wind = Window('sample')
+    base.run()
